939f8f1160b333bf06309ebe65c7a748c5dc2c8a
Tomasz Nieżurawski <tommaqs@gmail.com>
2022-09-26 09:11:50 +0200

    Add embroider

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { Webpack } = require('@embroider/webpack');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
@@ -19,6 +20,11 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
-  return app.toTree();
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+  });
 };
